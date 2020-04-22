9c258e9485eff1521fc8c1810d5172a4f01413b7
glbzzx0426 <glbzzx0426@163.com>
2020-04-22 13:40:30 +0800

    提交src目录

---
+++ src/Ztools.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martinzhang
+ * Date: 2020/4/22
+ * Time: 11:39
+ */
+namespace zphpsoft\tools;
+class Ztools
+{
+    public static function print345()
+    {
+        echo 'SSSSSS';
+    }
+}
